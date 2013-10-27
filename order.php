e83972f347cb69e12c53a2b523e755f103c6807a
Gergely Revay <gergely.revay@gmail.com>
2013-10-27 21:59:20 +0100

    Added descriptive title

--- order.php
+++ order.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>penmonkey:order</title>
+	<title>PENmonkey, a pen case to attach to your notebook</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<!-- <link rel="stylesheet" href="style.css" type="text/css" /> --> 
 	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
